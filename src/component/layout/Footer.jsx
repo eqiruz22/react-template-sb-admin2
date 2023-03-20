6ea5ff51170ba4d107bb26101c245de051979c2a
settclow <ahmadrifa11620@gmail.com>
2023-03-20 16:09:59 +0700

    footer copyright

--- src/component/layout/Footer.jsx
+++ src/component/layout/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
+    const date = new Date()
+    const year = date.getFullYear()
     return (
         <footer className="sticky-footer bg-white">
             <div className="container my-auto">
                 <div className="copyright text-center my-auto">
-                    <span>Copyright © Your Website 2022</span>
+                    <span>Copyright © Your Website 2022-{year}</span>
                 </div>
             </div>
         </footer>
